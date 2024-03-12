5a92743f7562c8073e5294634672c1daf3284c52
CarlHockey <thebicycarl@gmail.com>
2024-03-12 16:50:47 +1100

    intial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Compound interest calculator that calculates compounded interest value based on some user inputs
+
+// step 1 - function to calculate the final value of the compounded interest
+
+// step 2 - calculate the difference ie the effect the compounding had
+
+// step 3 - run function that prompts user for all required inputs
+
+// step 4 - return a statment with the financial breakdown
